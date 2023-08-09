0739b629cccb50544ed52d238c9c25f981891232
AytacSahin <haytacsahin@gmail.com>
2023-08-09 10:04:02 +0300

    all tasks done

--- src/com/workintech/mobile/main/Main.java
+++ src/com/workintech/mobile/main/Main.java
@@ -1,7 +1,33 @@
 package com.workintech.mobile.main;
 
+import com.workintech.mobile.model.Contact;
+import com.workintech.mobile.model.MobilePhone;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MobilePhone phone = new MobilePhone("1231231");
+        Contact ahmet = new Contact("Ahmet", "1231231");
+        phone.addNewContact(new Contact("Ayse", "5464665"));
+        phone.addNewContact(ahmet);
+        phone.addNewContact(new Contact("Cem", "1231232"));
+        phone.addNewContact(new Contact("Ali", "1231232"));
+        phone.addNewContact(new Contact("Cem", "1231233"));
+        phone.printContact();
+
+        System.out.println("Index: " + phone.findContact(ahmet));
+        System.out.println("Index with String: " + phone.findContact(ahmet.getName()));
+        System.out.println("Query Contact: " + phone.queryContact("Ayse"));
+        phone.removeContact(ahmet);
+        phone.printContact();
+
+        List<String> fruits = new LinkedList<>();
+        fruits.add("Banana".toLowerCase());
+        fruits.add("Apple".toLowerCase());
+
     }
+
 }
